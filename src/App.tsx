21ebe7a59396724c884130147e70ba28fac1f1c3
omgpiu <dyba@List.ru>
2021-03-11 21:19:31 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [messages, setMessages] = useState([{
+        message: 'hello Sasha',
+        id: '123',
+        user: {
+            id: '33333sdfsdfsdf',
+            name: 'Hello Sasha'
+        }
+    },
+        {
+            message: 'hello Tina',
+            id: '123',
+            user: {
+                id: '33333sdfsdfsdf',
+                name: 'Hello Tina'
+            }
+        }]);
+
+
+    return (
+        <div>
+
+
+            <div style={{
+                border: '1px solid black',
+                padding: '10px',
+                height: '300px',
+                overflow: 'scroll',
+                width: '300px'
+            }}>
+
+                {messages.map(e => {
+
+                    return (
+                        <div>
+                            <div>
+                                {e.message}
+                            </div>
+                            <div>{e.user.name}</div>
+                            <hr/>
+
+                        </div>
+                    )
+                        ;
+                })}
+
+
+            </div>
+            <div>
+                <textarea></textarea>
+                <button>Send</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
